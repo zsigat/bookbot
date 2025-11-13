4895e5dc9fc3f90c73a4c1434721d3833a47f3fb
tzsiga <zsiga0tamas@gmail.com>
2025-11-13 21:00:38 +0100

    create report in main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_word_count, get_char_count
+from stats import get_word_count, get_char_count, sort_dict
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -6,10 +6,21 @@ def get_book_text(filepath):
     return file_contents
 
 def main():
-    file_contents = get_book_text("books/frankenstein.txt")
+    filepath = "books/frankenstein.txt"
+    file_contents = get_book_text(filepath)
     word_count = get_word_count(file_contents)
-    print(f"Found {word_count} total words")
     char_count = get_char_count(file_contents)
-    print(char_count)
+    sorted_dict = sort_dict(char_count)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {filepath}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    for elem in sorted_dict:
+        if elem["char"].isalpha():
+            print(f"{elem['char']}: {elem['num']}")
+        else:
+            continue
+    print("============= END ===============")
 
 main()
