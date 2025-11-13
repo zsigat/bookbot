50b09fec4c1326e70fef71f72b8bd5d5dd6d5eb5
tzsiga <zsiga0tamas@gmail.com>
2025-11-13 20:28:53 +0100

    implement get_book_text() to main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greetings boots")
+def get_book_text(filepath):
+    with open(filepath) as f:
+        file_contents = f.read()
+    return file_contents
+
+def main():
+    file_contents = get_book_text("books/frankenstein.txt")
+    print(file_contents)
+
+main()
