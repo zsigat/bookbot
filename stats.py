a2c8fd32b8a5e0b764cfb1bbcf2b31c31e966b89
tzsiga <zsiga0tamas@gmail.com>
2025-11-13 20:59:44 +0100

    implement sort_dict() to stats.py

--- stats.py
+++ stats.py
@@ -10,4 +10,18 @@ def get_char_count(book):
         else:
             char_count[char.lower()] += 1
     return char_count
-        
+
+def sort_on(items):
+    return items["num"]
+
+def sort_dict(char_count):
+    sorted_dict = []
+    for char in char_count:
+        sorted_dict.append(
+            {
+                "char":char,
+                "num":char_count[char]
+            }
+        )
+    sorted_dict.sort(reverse=True, key=sort_on)
+    return sorted_dict
